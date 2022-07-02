6b0c3a0dc683fcfe795c31ca8fa3c5e8041d59a0
daniele.freitas <daniele.freitas@ecocentauro.com.br>
2022-07-02 16:19:19 -0300

    class conta

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta {
+    val agencia: String  = ""
+    val numero: String = ""
+    val saldo: Double = 0.0
+}
+fun deposito (valor: Double){
+
+}
+fun saqque (valor: Double){
+
+}
